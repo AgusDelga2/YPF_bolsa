4cb218c9ab72ee8f06237de6696510587c31ff95
AgusDelga2 <agustinadelgg@gmail.com>
2023-04-24 14:42:18 -0300

    Corrección del código

--- API/ypf_api_yahoofinance.py
+++ API/ypf_api_yahoofinance.py
@@ -14,7 +14,7 @@ ypf_daily_df = pd.DataFrame(ypf_daily)
 ypf_daily_df.to_csv('ypf_daily.csv')
 
 #Obtenemos datos semanales de YPF desde enero de 2019 a diciembre de 2022
-ypf_weekly= get_data("ypf", start_date="01/01/2019", end_date="12/12/2022", index_as_date = False, interval="1wk")
+ypf_weekly= get_data("YPFD.BA", start_date="01/01/2019", end_date="12/12/2022", index_as_date = False, interval="1wk")
 
 print(ypf_weekly.dtypes)
 print("------------- Datos semanales -------------")
