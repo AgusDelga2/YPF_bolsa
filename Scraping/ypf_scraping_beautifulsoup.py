feceff9a885c22706b7719226abc9be757ae5b16
AgusDelga2 <agustinadelgg@gmail.com>
2023-04-26 16:11:30 -0300

    Corrección

--- Scraping/ypf_scraping_beautifulsoup.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://es.investing.com/equities/ypf-sociedad"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-
-print(soup) 
