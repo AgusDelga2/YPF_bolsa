6552eeafef6649f815a7d64c9c0ab02559634cd8
AgusDelga2 <agustinadelgg@gmail.com>
2023-06-05 19:18:26 -0300

    Actualización del análisis de los datos

--- API/ypf_api_yahoofinance.py
+++ API/ypf_api_yahoofinance.py
@@ -1,6 +1,7 @@
 from yahoo_fin.stock_info import get_data
 import pandas as pd
 
+"""
 #Obtenemos datos diarios de YPF desde el 1 de enero de 2023 hasta el día de la fecha, 18 de mayo
 ypf_daily= get_data("YPFD.BA", start_date="01/01/2023", end_date="05/18/2023", index_as_date = False, interval="1d")
 
@@ -14,9 +15,24 @@ print(ypf_daily.tail())
 ypf_daily_df = pd.DataFrame(ypf_daily)
 
 #Guardamos el archivo csv
-ypf_daily_df.to_csv('ypf_daily_23_api.csv')
+ypf_daily_df.to_csv('ypf_daily_23_api.csv')"""
 
 #------------------------------------------------------------------------------------------------------
+#Obtenemos datos semanales de YPF desde enero de 2023 hasta la actualidad
+ypf_weekly_23= get_data("YPFD.BA", start_date="01/01/2023", end_date="05/26/2023", index_as_date = False, interval="1wk")
+
+print(ypf_weekly_23.dtypes)
+print("------------- Datos semanales de 2023 -------------")
+print(ypf_weekly_23.head())
+print(".......................................................................")
+print(ypf_weekly_23.tail())
+#convertimos a df
+ypf_weekly_23_df = pd.DataFrame(ypf_weekly_23)
+
+#Guardamos el archivo csv
+ypf_weekly_23_df.to_csv('ypf_weekly_23_api.csv')
+
+"""#------------------------------------------------------------------------------------------------------
 #Obtenemos datos semanales de YPF desde enero de 2019 a diciembre de 2022
 ypf_weekly= get_data("YPFD.BA", start_date="01/01/2019", end_date="31/12/2022", index_as_date = False, interval="1wk")
 
@@ -30,3 +46,4 @@ ypf_weekly_df = pd.DataFrame(ypf_weekly)
 
 #Guardamos el archivo csv
 ypf_weekly_df.to_csv('ypf_weekly_19-22_api.csv')
+"""
